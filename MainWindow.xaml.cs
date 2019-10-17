ae43307286b1bd11c7007e74ee8860fce1dc29f7
Артем Сухин <artem2802200@live.com>
2019-10-17 16:42:06 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using TcpSendFiles;
+
+namespace FTrns
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[,] ip = new string[255, 2];
+        bool c = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string PingCheck(string A)
+        {
+            Ping ping = new Ping();
+            PingReply pingReply = ping.Send(A, 500);
+            if (pingReply.Status == IPStatus.Success)
+            {
+                return A;
+            }
+            return null;
+        }
+
+        void Start(string ipnum, int c, int j)
+        {
+            string t;
+            int i = 0;
+            string ipn;
+            for (; c <= j; ++c)
+            {
+                ipn = ipnum + c;
+                t = PingCheck(ipn);
+                if (t != null)
+                {
+                    ip[i, 2] = t;
+                    if (ip[i, 1] == null)
+                    {
+
+                    }
+                    
+                    
+                    i++;
+                }
+                ipn = null;
+            }
+            Thread.Sleep(10);
+        }
+
+        string Adapters()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                GatewayIPAddressInformationCollection addresses = adapterProperties.GatewayAddresses;
+                if (addresses.Count > 0)
+                {
+                    Console.WriteLine(adapter.Description);
+                    foreach (GatewayIPAddressInformation address in addresses)
+                    {
+                        if (address.Address.ToString().StartsWith("192.168."))
+                        {
+                            return address.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        async void StartAsync(string ipn, int a, int b)
+        {
+            await Task.Run(() => Start(ipn, a, b));
+        }
+
+        private void Check(object sender, EventArgs e)
+        {
+            if (c)
+            {
+                TcpModule ts = new TcpModule();
+                ts.Start();
+                c = false;
+            }
+            string ipnum = Adapters().Substring(0, 10);
+            StartAsync(ipnum, 0, 255);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //_tcpmodule.SendFileName = dlg.FileName;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //_tcpmodule.ConnectClient((string)list1.SelectedItem);
+            //Thread t = new Thread(_tcpmodule.SendData);
+            //t.Start();
+        }
+    }
+}
---
+++ tcpmodule.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.Text;
+using System.Drawing;
+
+
+namespace TcpSendFiles
+{
+    class TcpModule
+    {
+        private Socket _serverSocket;
+        private int _port = 15000;
+
+        private class ConnectionInfo
+        {
+            public Socket Socket;
+            public Thread Thread;
+        }
+
+        private Thread _acceptThread;
+        private List<ConnectionInfo> _connections = new List<ConnectionInfo>();
+
+        public void Start()
+        {
+            SetupServerSocket();
+            _acceptThread = new Thread(AcceptConnections);
+            _acceptThread.IsBackground = true;
+            _acceptThread.Start();
+        }
+
+        private void SetupServerSocket()
+        {
+            // Получаем информацию о локальном компьютере
+            IPHostEntry localMachineInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPEndPoint myEndpoint = new IPEndPoint(localMachineInfo.AddressList[0], _port);
+
+            // Создаем сокет, привязываем его к адресу
+            // и начинаем прослушивание
+            _serverSocket = new Socket(myEndpoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _serverSocket.Bind(myEndpoint);
+            _serverSocket.Listen((int)SocketOptionName.MaxConnections);
+        }
+
+        private void AcceptConnections()
+        {
+            while (true)
+            {
+                // Принимаем соединение
+                Socket socket = _serverSocket.Accept();
+                ConnectionInfo connection = new ConnectionInfo();
+                connection.Socket = socket;
+
+                // Создаем поток для получения данных
+                connection.Thread = new Thread(ProcessConnection);
+                connection.Thread.IsBackground = true;
+                connection.Thread.Start(connection);
+
+                // Сохраняем сокет
+                lock (_connections) _connections.Add(connection);
+            }
+        }
+
+        private void ProcessConnection(object state)
+        {
+            ConnectionInfo connection = (ConnectionInfo)state;
+            byte[] buffer = new byte[255];
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = connection.Socket.Receive(
+                        buffer);
+                    if (bytesRead > 0)
+                    {
+                        lock (_connections)
+                        {
+                            foreach (ConnectionInfo conn in
+                                _connections)
+                            {
+                                if (conn != connection)
+                                {
+                                    conn.Socket.Send(
+                                        buffer, bytesRead,
+                                        SocketFlags.None);
+                                }
+                            }
+                        }
+                    }
+                    else if (bytesRead == 0) return;
+                }
+            }
+            catch (SocketException exc)
+            {
+                Console.WriteLine("Socket exception: " +
+                    exc.SocketErrorCode);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Exception: " + exc);
+            }
+            finally
+            {
+                connection.Socket.Close();
+                lock (_connections) _connections.Remove(
+                    connection);
+            }
+        }
+    }
+}
